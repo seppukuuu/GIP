5432fc657aae093a2f902f2a68fbb46466f4e579
seppukuuu <S3PP3again@hotmail.com>
2023-01-31 13:54:58 +0100

    new

--- root/GIP WEBSITE/script.js
+++ root/GIP WEBSITE/script.js
@@ -1,74 +1,129 @@
-const carousel1 = document.querySelector('.carousel1');
-const carousel2 = document.querySelector('.carousel2');
-const itemsContainer1 = carousel1.querySelector('.carousel-items');
-const itemsContainer2 = carousel2.querySelector('.carousel-items');
-const items1 = Array.from(itemsContainer1.children);
-const items2 = Array.from(itemsContainer2.children);
-const prevButton1 = carousel1.querySelector('.carousel-button-prev');
-const nextButton1 = carousel1.querySelector('.carousel-button-next');
-const prevButton2 = carousel2.querySelector('.carousel-button-prev');
-const nextButton2 = carousel2.querySelector('.carousel-button-next');
-
-let viewportHeight = window.innerHeight;
-let viewportWidth = window.innerWidth;
-
-let currentItemIndex1 = 0;
-let currentItemIndex2 = 0;
-let itemsPerPage1 = 5.01;
-let itemsPerPage2 = 5.01;
-
-
-
-function goToItem1(index) {
-  if (index < 0) {
-    index = items1.length - itemsPerPage1;
-  } else if (index > items1.length - itemsPerPage1) {
-    index = 0;
-  }
-  itemsContainer1.style.transition = 'transform 0.5s ease';
-  itemsContainer1.style.transform = `translateX(-${index * (100 / itemsPerPage1)}%)`;
-  currentItemIndex1 = index;
-}
-
-function goToItem2(index) {
-  if (index < 0) {
-    index = items2.length - itemsPerPage2;
-  } else if (index > items2.length - itemsPerPage2) {
-    index = 0;
-  }
-  itemsContainer2.style.transition = 'transform 0.5s ease';
-  itemsContainer2.style.transform = `translateX(-${index * (100 / itemsPerPage2)}%)`;
-  currentItemIndex2 = index;
-}
-
-prevButton1.addEventListener('click', () => {
-  goToItem1(currentItemIndex1 - 1);
-});
-
-nextButton1.addEventListener('click', () => {
-  goToItem1(currentItemIndex1 + 1);
-});
-
-prevButton2.addEventListener('click', () => {
-  goToItem2(currentItemIndex2 - 1);
-});
-
-nextButton2.addEventListener('click', () => {
-  goToItem2(currentItemIndex2 + 1);
-});
-
-goToItem1(currentItemIndex1);
-goToItem2(currentItemIndex2);
-
-window.addEventListener('resize', () => {
-  if (window.innerWidth < 800) {
-    itemsPerPage1 = 2;
-    itemsPerPage2 = 2;
+document.addEventListener("click", e => {
+  let handle
+  if (e.target.matches(".handle")) {
+    handle = e.target
   } else {
-    itemsPerPage1 = 6;
-    itemsPerPage2 = 6;
+    handle = e.target.closest(".handle")
   }
-  goToItem1(currentItemIndex1);
-  goToItem2(currentItemIndex2);
+  if (handle != null) onHandleClick(handle)
+})
+
+const throttleProgressBar = throttle(() => {
+  document.querySelectorAll(".progress-bar").forEach(calculateProgressBar)
+}, 250)
+window.addEventListener("resize", throttleProgressBar)
+
+document.querySelectorAll(".progress-bar").forEach(calculateProgressBar)
+
+function calculateProgressBar(progressBar) {
+  progressBar.innerHTML = ""
+  const slider = progressBar.closest(".row").querySelector(".slider")
+  const itemCount = slider.children.length
+  const itemsPerScreen = parseInt(
+    getComputedStyle(slider).getPropertyValue("--items-per-screen")
+  )
+  let sliderIndex = parseInt(
+    getComputedStyle(slider).getPropertyValue("--slider-index")
+  )
+  const progressBarItemCount = Math.ceil(itemCount / itemsPerScreen)
+
+  if (sliderIndex >= progressBarItemCount) {
+    slider.style.setProperty("--slider-index", progressBarItemCount - 1)
+    sliderIndex = progressBarItemCount - 1
+  }
+
+  for (let i = 0; i < progressBarItemCount; i++) {
+    const barItem = document.createElement("div")
+    barItem.classList.add("progress-item")
+    if (i === sliderIndex) {
+      barItem.classList.add("active")
+    }
+    progressBar.append(barItem)
+  }
+}
+
+function onHandleClick(handle) {
+  const progressBar = handle.closest(".row").querySelector(".progress-bar")
+  const slider = handle.closest(".container").querySelector(".slider")
+  const sliderIndex = parseInt(
+    getComputedStyle(slider).getPropertyValue("--slider-index")
+  )
+  const progressBarItemCount = progressBar.children.length
+  if (handle.classList.contains("left-handle")) {
+    if (sliderIndex - 1 < 0) {
+      slider.style.setProperty("--slider-index", progressBarItemCount - 1)
+      progressBar.children[sliderIndex].classList.remove("active")
+      progressBar.children[progressBarItemCount - 1].classList.add("active")
+    } else {
+      slider.style.setProperty("--slider-index", sliderIndex - 1)
+      progressBar.children[sliderIndex].classList.remove("active")
+      progressBar.children[sliderIndex - 1].classList.add("active")
+    }
+  }
+
+  if (handle.classList.contains("right-handle")) {
+    if (sliderIndex + 1 >= progressBarItemCount) {
+      slider.style.setProperty("--slider-index", 0)
+      progressBar.children[sliderIndex].classList.remove("active")
+      progressBar.children[0].classList.add("active")
+    } else {
+      slider.style.setProperty("--slider-index", sliderIndex + 1)
+      progressBar.children[sliderIndex].classList.remove("active")
+      progressBar.children[sliderIndex + 1].classList.add("active")
+    }
+  }
+}
+
+function throttle(cb, delay = 1000) {
+  let shouldWait = false
+  let waitingArgs
+  const timeoutFunc = () => {
+    if (waitingArgs == null) {
+      shouldWait = false
+    } else {
+      cb(...waitingArgs)
+      waitingArgs = null
+      setTimeout(timeoutFunc, delay)
+    }
+  }
+
+  return (...args) => {
+    if (shouldWait) {
+      waitingArgs = args
+      return
+    }
+
+    cb(...args)
+    shouldWait = true
+    setTimeout(timeoutFunc, delay)
+  }
+}
+
+/* Function to open the overlay */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Function to close the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
+const searchIcon = document.querySelector('.search-icon');
+const searchInput = document.querySelector('.search-input');
+
+searchIcon.addEventListener('click', () => {
+  if (searchInput.style.width === '0px') {
+    searchInput.style.width = '250px';
+  } else {
+    searchInput.style.width = '0';
+  }
 });
 
+const burger = document.querySelector('.burger-menu');
+const rightNav = document.querySelector('.right-nav-items');
+
+burger.addEventListener('click', () => {
+  rightNav.classList.toggle('show');
+});
